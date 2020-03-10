759c73aeae1dbd15a18b3f7d53224eda96c9495e
TheSandDoctor <majorjohn1@mail.com>
2020-03-09 21:07:42 -0700

    Fix logging, tweaks

--- remove_blocked.py
+++ remove_blocked.py
@@ -5,14 +5,14 @@ if __name__ == "__main__":
     site = pywikibot.Site(fam="wikipedia", code="en", user="TheSandBot")
     pattern = r'(?:User talk:)([^/\n]+)'
     for page in pywikibot.Category(site, "Wikipedia usernames with possible policy issues").articles():
-        with open("log.txt", 'a+') as f:
-            f.write(str(page.title()) + "\n")
         if page.title() == "Template:Uw-corpname":
             continue
         m = re.search(pattern, str(page.title()))
         user_raw = m.group(1)
         user = pywikibot.User(site, user_raw)
         if user.isBlocked():
+            with open("block_removed_mar_9_2020.txt", 'a+') as f:
+                f.write(str(page.title()) + "\n")
             page.text = page.text.replace("[[Category:Wikipedia usernames with possible policy issues|{{PAGENAME}}]]",
                                           "")
             page.save(
