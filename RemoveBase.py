23f6dc0b7dc66b585354c2cf55f540da33bcbd48
TheSandDoctor <majorjohn1@mail.com>
2020-04-08 16:54:04 -0700

    Update to class based structure, add locked user support
    Closes T132
    Closes T133

--- RemoveBase.py
+++ RemoveBase.py
@@ -1,14 +1,20 @@
 import pywikibot
 import re
+import requests
 
 
 class RemoveBase:
     def __init__(self, log_name, category):
         self.pattern = r'(?:User talk:)([^/\n]+)'
-        self.site = pywikibot.Site(fam="wikipedia", code="en", user="TheSandBot")
+        self.site = pywikibot.Site()  # fam="wikipedia", code="en", user="TheSandBot")
         self.count = 0
         self.log_filename = log_name
-        self.category = pywikibot.Category(self.site, category).articles()
+        self.session = requests.Session()
+        try:
+            self.category = pywikibot.Category(self.site, category).articles()
+        except pywikibot.InvalidTitle:
+            print("\nBad category title")
+            exit()
         self.cat_name = category
 
     def category_remove(self, target: str, page: pywikibot.Page) -> None:
@@ -25,13 +31,21 @@ class RemoveBase:
         user_raw = m.group(1)
         return pywikibot.User(self.site, user_raw)
 
-    def run(self):
-        pass
+    def isLocked(self, username):
+        params = {
+            "action": "query",
+            "format": "json",
+            "meta": "globaluserinfo",
+            "guiuser": str(username),
+            "guiprop": "groups|merged|unattached"
+        }
+        result = self.session.get(url="https://en.wikipedia.org/w/api.php", params=params)
+        return 'locked' in result.json()["query"]["globaluserinfo"]
 
     def log(self, page):
         with open(self.log_filename, 'a+') as f:
-            self.count += 1
-            f.write(str(self.count) + " " + str(page.title()) + "\n")
+            # self.count += 1
+            f.write(str(page.title()) + "\n")
 
 
 class RemoveBlocked(RemoveBase):
@@ -45,30 +59,48 @@ class RemoveBlocked(RemoveBase):
             if page.title() == "Template:Uw-corpname":
                 continue
             user = self.generate_user(page)
-            if user.isBlocked():
+
+            if self.isLocked(user.username):
+                self.log(page)
+                self.category_remove(self.target, page)
+                page.save(
+                    summary="Removing " + self.cat_name + " as user is locked." +
+                            " ([[Wikipedia:Bots/Requests for approval/" + self.brfa + "|BRFA]])", minor=True,
+                    botflag=True, force=True)
+                print("Saved " + str(page.title()))
+            elif user.isBlocked():
                 self.log(page)
                 self.category_remove(self.target, page)
                 page.save(
                     summary="Removing " + self.cat_name + " as user is blocked." +
-                            " ([[" + self.brfa + "|BRFA]])", minor=True,
+                            " ([[Wikipedia:Bots/Requests for approval/" + self.brfa + "|BRFA]])", minor=True,
                     botflag=True, force=True)
                 print("Saved " + str(page.title()))
 
 
 class RemoveUnblocked(RemoveBase):
-    def __init__(self, log_name, category, target, brfa):
+    def __init__(self, log_name, category, target, backup_target, brfa):
         super(RemoveUnblocked, self).__init__(log_name, category)
         self.target = target
+        self.backup_target = backup_target
         self.brfa = brfa
 
     def run(self):
         for page in self.category:
             if page.title() == "Template:Uw-corpname":
                 continue
+            # print(page.title())
+            if page.title()[:10] != "User talk:":
+                print("NOT RIGHT FORMAT " + page.title())
+                continue
             user = self.generate_user(page)
             if not user.isBlocked():
                 self.log(page)
+                old_t = page.text
                 self.category_remove(self.target, page)
+                if page.text == old_t:
+                    print("Move to backup")
+                    self.category_remove(self.backup_target, page)
                 page.save(
                     summary="Removing " + self.cat_name + " as user is unblocked." +
                             " ([[Wikipedia:Bots/Requests for approval/" + self.brfa + "|BRFA]])", minor=True,
--- remove_blocked2.py
+++ remove_blocked2.py
@@ -1,8 +1,11 @@
 from RemoveBase import RemoveBlocked
 
 if __name__ == "__main__":
-    rmBlocked = RemoveBlocked(log_name="block_removed_mar_14_2020.txt",
-                              category="[[:Category:Wikipedia usernames with possible policy issues]]",
+    rmBlocked = RemoveBlocked(log_name="block_removed_apr_3_2020.txt",
+                              category="Wikipedia usernames with possible policy issues",
                               target="[[Category:Wikipedia usernames with possible policy issues|{{PAGENAME}}]]",
                               brfa="TheSandBot 6")
-    rmBlocked.run()
+    try:
+        rmBlocked.run()
+    except KeyboardInterrupt:
+        print("\n")
