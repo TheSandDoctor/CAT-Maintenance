afc70654ae623bd7e9d74f95ed0be7f8955ad8cb
TheSandDoctor <majorjohn1@mail.com>
2020-04-08 17:23:46 -0700

    Add unblock functionality for TSB 7

--- remove_unblocked.py
+++ remove_unblocked.py
@@ -2,7 +2,11 @@ from RemoveBase import RemoveUnblocked
 
 if __name__ == "__main__":
     rmBlocked = RemoveUnblocked(log_name="unblocked_removed_mar_17_2020.txt",
-                              category="[[:Category:Wikipedians who are indefinitely blocked for promotional user names]]",
-                              target="[[Category:Wikipedians who are indefinitely blocked for promotional user names|{{PAGENAME}}]]",
-                              brfa="TheSandBot 7")
-    rmBlocked.run()
+                                category="Wikipedians who are indefinitely blocked for promotional user names",
+                                target="[[Category:Wikipedians who are indefinitely blocked for promotional user names|{{PAGENAME}}]]",
+                                backup_target="[[Category:Wikipedians who are indefinitely blocked for promotional user names||{{PAGENAME}}]]",
+                                brfa="TheSandBot 7")
+    try:
+        rmBlocked.run()
+    except KeyboardInterrupt:
+        print("\n")
