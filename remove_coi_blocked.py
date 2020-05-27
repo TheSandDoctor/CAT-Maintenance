ee97d4abfadefe1b8a14f0c827393ac3a0e917e2
TheSandDoctor <majorjohn1@mail.com>
2020-05-27 08:19:14 -0700

    Add trial version

--- remove_coi_blocked.py
+++ remove_coi_blocked.py
@@ -4,7 +4,7 @@ if __name__ == "__main__":
     rmBlocked = RemoveBlocked(log_name="block_coi_removed_may_26_2020.txt",
                               category="User talk pages with conflict of interest notices",
                               target="[[Category:User talk pages with conflict of interest notices|{{PAGENAME}}]]",
-                              brfa="TheSandBot 10", trial=True)
+                              brfa="TheSandBot 10", trial=True, count=36)
     try:
         rmBlocked.run()
     except KeyboardInterrupt:
