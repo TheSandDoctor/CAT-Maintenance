309abeddba4ce19131d0c97979ca81857eed2b4e
TheSandDoctor <majorjohn1@mail.com>
2020-05-27 07:40:59 -0700

    Add TSB 10

---
+++ remove_coi_blocked.py
@@ -0,0 +1,11 @@
+from RemoveBase import RemoveBlocked
+
+if __name__ == "__main__":
+    rmBlocked = RemoveBlocked(log_name="block_coi_removed_may_26_2020.txt",
+                              category="User talk pages with conflict of interest notices",
+                              target="[[Category:User talk pages with conflict of interest notices|{{PAGENAME}}]]",
+                              brfa="TheSandBot 10", trial=True)
+    try:
+        rmBlocked.run()
+    except KeyboardInterrupt:
+        print("\n")
