2ec5a36cdb6d200a62109238c712a7bec4ed124a
TheSandDoctor <ktwilson40+tsd@gmail.com>
2025-11-30 20:27:01 -0800

    Tweak remove_coi_blocked logic to add better error handling

--- remove_coi_blocked.py
+++ remove_coi_blocked.py
@@ -1,5 +1,6 @@
 from RemoveBase import RemoveBlocked
 from datetime import date
+from requests import ConnectionError
 
 if __name__ == "__main__":
     today = date.today()
@@ -7,8 +8,19 @@ if __name__ == "__main__":
                                     today.strftime("%b_%d_%Y") + ".txt",
                               category="User talk pages with conflict of interest notices",
                               target="[[Category:User talk pages with conflict of interest notices|{{PAGENAME}}]]",
-                              brfa="TheSandBot 10", span="2019-02-02T00:00:00Z")
-    try:
-        rmBlocked.run()
-    except KeyboardInterrupt:
-        print("\n")
+                              brfa="TheSandBot 10",
+span="2024-12-30T00:00:00Z")
+
+    num_failures = 0
+    while True:
+        try:
+            rmBlocked.run()
+            break
+        except KeyboardInterrupt:
+            print("\n")
+            break
+        except ConnectionError:
+            if num_failures > 30:
+                raise Exception("Unable to complete, ConnectionError")
+            else:
+                num_failures += 1
