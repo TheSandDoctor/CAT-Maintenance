e10840d9645ca7435ef6470adfa0893c18b5126f
TheSandDoctor <ktwilson40+tsd@gmail.com>
2026-01-18 12:04:01 -0800

    Some cleanup

--- remove_blocked.py
+++ remove_blocked.py
@@ -15,13 +15,3 @@ if __name__ == "__main__":
         rmBlocked.run()
     except KeyboardInterrupt:
         print("\n")
-    # # months = ['January 2025', 'February 2025', 'March 2025', 'April 2025', 'May 2025', 'June 2025', 'July 2025', "August 2025", "September 2025", "October 2025"]#, "November 2024", "December 2024"]
-    # months = ['December 2025']
-    # for month in months:
-    #     print(f"Running {month}")
-    #     rmBlocked = RemoveBlocked(
-    #         log_name="block_removed_" + today.strftime("%b_%d_%Y") + ".txt",
-    #         category=f"Wikipedia usernames with possible policy issues from {month}",
-    #         target=f"[[Category:Wikipedia usernames with possible policy issues from {month}|{{{{PAGENAME}}}}]]",
-    #         brfa="TheSandBot 6"
-    #     )
